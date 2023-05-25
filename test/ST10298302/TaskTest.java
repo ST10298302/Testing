e1ca888d75331e7a8963ccc88cec94c4e8c5de66
ST10298302 <126656401+ST10298302@users.noreply.github.com>
2023-05-25 14:47:54 +0200

    Update TaskTest.java

--- test/ST10298302/TaskTest.java
+++ test/ST10298302/TaskTest.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 /**
  *
